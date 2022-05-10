838a333751a091f858a670d7337c8b2e6f2a34eb
Candra Sudirman <candra_sudirman@bridgenote.id>
2022-05-10 12:59:15 +0700

    Optimize unit test

--- tests/Unit/HttpLoggerTest.php
+++ tests/Unit/HttpLoggerTest.php
@@ -10,51 +10,33 @@ use Illuminate\Http\Response;
 class HttpLoggerTest extends TestCase
 {
 	/**
-	 * Instance of DefaultHttpLogger.
-	 *
-	 * @var \Cndrsdrmn\HttpLogger\DefaultHttpLogger
-	 */
-	protected $logger;
-	
-	/**
 	 * A test skipped endpoints write log.
 	 *
 	 * @return void
 	 */
 	public function test_skipped_endpoints_write_log()
 	{
-		$configs = array_merge($this->app->config->get('http-logger'), [
-			'skip_endpoints' => ['/foo/*'],
-		]);
-		
-		$start   = microtime(true);
+		$configs = ['skip_endpoints' => ['/foo/*']];
 		$request = Request::create('foo/not-important', 'POST', ['foo' => 'bar']);
 		
-		$response = new Response();
-		$interval = microtime(true) - $start;
-		$logger   = new DefaultHttpLogger($configs);
-		
-		$logger->write($request, $response, $interval);
+		$this->write($request, null, $configs);
 		
 		$this->assertFileDoesNotExist($this->fileLogger());
 	}
 	
+	/**
+	 * A test skipped ips write log.
+	 *
+	 * @return void
+	 */
 	public function test_skipped_ips_write_log()
 	{
-		$configs = array_merge($this->app->config->get('http-logger'), [
-			'skip_ips' => ['192.168.*'],
-		]);
-		
-		$start   = microtime(true);
+		$configs = ['skip_ips' => ['192.168.*']];
 		$request = Request::create('not-important', 'POST', ['foo' => 'bar'], [], [], [
 			'REMOTE_ADDR' => '192.168.1.1',
 		]);
 		
-		$response = new Response();
-		$interval = microtime(true) - $start;
-		$logger   = new DefaultHttpLogger($configs);
-		
-		$logger->write($request, $response, $interval);
+		$this->write($request, null, $configs);
 		
 		$this->assertFileDoesNotExist($this->fileLogger());
 	}
@@ -66,23 +48,31 @@ class HttpLoggerTest extends TestCase
 	 */
 	public function test_write_log()
 	{
-		$start    = microtime(true);
-		$request  = Request::create('not-important', 'POST', ['foo' => 'bar']);
-		$response = new Response();
-		$interval = microtime(true) - $start;
-		
-		$this->logger->write($request, $response, $interval);
+		$this->write(Request::create('not-important', 'POST', ['foo' => 'bar']));
 		
 		$this->assertStringContainsString('"foo":"bar"', $this->readFileLogger());
 	}
 	
 	/**
-	 * {@inheritDoc}
+	 * Write default http logger.
+	 *
+	 * @param  \Illuminate\Http\Request|null  $request
+	 * @param  \Illuminate\Http\Response|null $response
+	 * @param  array                          $configs
+	 * @return void
 	 */
-	protected function setUp(): void
+	protected function write(Request $request = null, Response $response = null, array $configs = [])
 	{
-		parent::setUp();
+		$configs = array_merge($this->app->config->get('http-logger'), $configs);
 		
-		$this->logger = new DefaultHttpLogger($this->app->config->get('http-logger'));
+		$start = microtime(true);
+		
+		$request  = $request ?? Request::create('not-important');
+		$response = $response ?? new Response();
+		
+		$interval = microtime(true) - $start;
+		
+		$logger = new DefaultHttpLogger($configs);
+		$logger->write($request, $response, $interval);
 	}
 }
